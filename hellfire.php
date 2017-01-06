df70cd0854fbce9f6f0b7f8951ee6691427b982c
Krzysztof Kubanek <kkubanek@pureholidayhomes.com>
2017-01-06 22:57:32 +0100

    main scripts made executable

--- hellfire.php
+++ hellfire.php
@@ -2,7 +2,7 @@
 <?php
 
 function setup() {
-    $sensor1 = new TemperatureSensor('Sensor1', '/sys/bus/w1/devices/28-0000084a49a8/w1_slave');
+    $sensor1 = new \Hellfire\Sensor\TemperatureSensor('Sensor1', '/sys/bus/w1/devices/28-0000084a49a8/w1_slave');
 
 }
 
